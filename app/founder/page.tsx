b480c7178cc506012c702eaa3acd99f53354ebe1
syedalihaider-dev <aleehaiderbalti@gmail.com>
2026-08-22 01:41:30 +0500

    Add New Page

--- app/founder/page.tsx
+++ app/founder/page.tsx
@@ -1,21 +1,23 @@
-import React from 'react';
-import Header from '../../components/Header';
-import Footer from '../../components/Footer';
-import FounderHero from '../../components/founder/FounderHero/FounderHero';
-import FounderStory from '../../components/founder/FounderStory/FounderStory';
-import FounderStats from '../../components/founder/FounderStats/FounderStats';
-import FounderStatements from '../../components/founder/FounderStatements/FounderStatements';
-import FounderJourney from '../../components/founder/FounderJourney/FounderJourney';
-import FounderTeam from '../../components/founder/FounderTeam/FounderTeam';
-import FounderRecognition from '../../components/founder/FounderRecognition/FounderRecognition';
-import FounderDevices from '../../components/founder/FounderDevices/FounderDevices';
-import FounderTestimonials from '../../components/founder/FounderTestimonials/FounderTestimonials';
-import FounderFaqs from '../../components/founder/FounderFaqs/FounderFaqs';
-import RouteLayout from '../../components/RouteLayout';
+import FounderHero from '@/components/founder/FounderHero/FounderHero';
+import FounderStory from '@/components/founder/FounderStory/FounderStory';
+import FounderStats from '@/components/founder/FounderStats/FounderStats';
+import FounderStatements from '@/components/founder/FounderStatements/FounderStatements';
+import FounderJourney from '@/components/founder/FounderJourney/FounderJourney';
+import FounderTeam from '@/components/founder/FounderTeam/FounderTeam';
+import FounderRecognition from '@/components/founder/FounderRecognition/FounderRecognition';
+import FounderDevices from '@/components/founder/FounderDevices/FounderDevices';
+import FounderTestimonials from '@/components/founder/FounderTestimonials/FounderTestimonials';
+import FounderFaqs from '@/components/founder/FounderFaqs/FounderFaqs';
+
+import RouteLayout from '@/components/RouteLayout';
 
 export const metadata = {
   title: 'Founder | Cobweb Games',
   description: 'The story behind Cobweb Games.',
+  robots: {
+    index: false,
+    follow: false,
+  },
 };
 
 export default function FounderPage() {
