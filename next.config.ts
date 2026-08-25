1cf8eb82bb1f41d88d712063d60e0ee548a15f2e
syedalihaider-dev <aleehaiderbalti@gmail.com>
2026-08-26 04:32:20 +0500

    Replace NextConfig Content

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,26 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "cobwebgames.vercel.app",
+          },
+        ],
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "noindex, nofollow",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
